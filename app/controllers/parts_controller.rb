560d3f788ff78ff48c3be0231215e4a71e85cfd1
Kara Brightwell <kara@153.io>
2020-12-20 13:51:06 +0000

    not-awful design

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,14 +1,13 @@
 class PartsController < ApplicationController
-  before_action :set_part, only: [:show, :edit, :update, :destroy]
+  before_action :set_part, only: %i[show edit update destroy]
 
   # GET /parts
   def index
-    @parts = Part.order(:value)
+    @parts = Part.where.not(quantity: 0).order(:value)
   end
 
   # GET /parts/1
-  def show
-  end
+  def show; end
 
   # GET /parts/new
   def new
@@ -16,15 +15,14 @@ class PartsController < ApplicationController
   end
 
   # GET /parts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /parts
   def create
     @part = Part.new(part_params)
 
     if @part.save
-      redirect_to @part, notice: "Part was successfully created."
+      redirect_to @part, notice: 'Part was successfully created.'
     else
       render :new
     end
@@ -33,7 +31,7 @@ class PartsController < ApplicationController
   # PATCH/PUT /parts/1
   def update
     if @part.update(part_params)
-      redirect_to @part, notice: "Part was successfully updated."
+      redirect_to @part, notice: 'Part was successfully updated.'
     else
       render :edit
     end
@@ -42,7 +40,7 @@ class PartsController < ApplicationController
   # DELETE /parts/1
   def destroy
     @part.destroy
-    redirect_to parts_url, notice: "Part was successfully destroyed."
+    redirect_to parts_url, notice: 'Part was successfully destroyed.'
   end
 
   private

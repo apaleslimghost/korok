3bba0edfc66bf8063ed08998ebbb15aaaba59ecc
Kara Brightwell <kara@153.io>
2021-02-03 21:29:12 +0000

    add json endpoint

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - [x] negative remaining quantities look weird
 - [ ] inputting lots of the same type of thing is clunky
 - [ ] default units for things
-- [ ] long cards look bad
+- [x] long cards look bad
--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,4 +1,12 @@
 class RequirementsController < ApplicationController
+  def index
+    project = Project.find(params[:project_id])
+
+    respond_to do |format|
+      format.json { render json: project.to_json(include: { requirements: { include: :part } }) }
+    end
+  end
+
   def create
     project = Project.find(params[:project_id])
 

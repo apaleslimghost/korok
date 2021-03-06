5755a4da1c2a1c8ed8d6b6dc49ebb1d92892ee52
Kara Brightwell <kara@153.io>
2021-03-06 15:56:33 +0000

    splash page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,101 @@
 class HomeController < ApplicationController
+  layout 'base'
+
   def index
     if current_user
       redirect_to projects_path
     end
+
+    @project = Project.new(name: 'Atari Punk Console')
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Resistor',
+        value: '1kohm',
+        quantity: 1
+      ),
+      quantity: 2
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Resistor',
+        value: '10kohm',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Resistor',
+        value: '15kohm',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Resistor',
+        value: '4.7kohm',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Resistor',
+        value: '100kohm',
+        quantity: 3
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'IC',
+        value: '555 Timer',
+        quantity: 1
+      ),
+      quantity: 2
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Electrolytic Capacitor',
+        value: '10μF',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Ceramic Capacitor',
+        value: '100nF',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Ceramic Capacitor',
+        value: '10nF',
+        quantity: 1
+      ),
+      quantity: 1
+    )
+    @project.requirements.new(
+      part: Part.new(
+        part_type: 'Potentiometer',
+        value: '500kohm',
+        quantity: 0
+      ),
+      quantity: 2
+    )
+
+    @ready_project = Project.new(name: 'Super Simple Oscillator')
+    @ready_project.requirements.new(
+      part: Part.new(
+        part_type: 'doesnt matter',
+        value: '',
+        quantity: 1
+      ),
+      quantity: 1
+    )
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+   def extends(layout, &block)
+      layout = layout.to_s
+      layout = "layouts/#{layout}" unless layout.include?('/')
+      @view_flow.get(:layout).replace capture(&block)
+      render template: layout
+    end
 end
